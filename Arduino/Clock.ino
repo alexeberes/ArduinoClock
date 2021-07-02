6dbd415497bc49d9841fcc0f70cc7097df1ed74b
alexeberes <75806636+alexeberes@users.noreply.github.com>
2021-07-02 11:46:37 -0400

    Updated Code

--- Arduino/Clock.ino
+++ Arduino/Clock.ino
@@ -9,48 +9,26 @@ unsigned long start_time;
 unsigned long elapsed_time;
 const unsigned long second_length = 1000;
 
-int hr = 0;
-int min = 0;
+int hr = 11;
+int min = 46;
 int sec = 0;
 
-void recalibrateTime(){
-    int byte4 = 0;
-    int byte3 = 0;
-    int byte2 = 0;
-    int byte1 = 0;
-    if(Serial.available() > 0){
-        byte4 = Serial.read();
-    }
-    if(Serial.available() > 0){
-        byte3 = Serial.read();
-    }
-    if(Serial.available() > 0){
-        byte2 = Serial.read();
-    }
-    if(Serial.available() > 0){
-        byte1 = Serial.read();
-    }
-    hr = byte1 * 10 + byte2;
-    min = byte3 * 10 + byte4;
-}
 
 void setup(){
     Serial.begin(9600);
     delay(1000);
-    display.setBrightness(7);
+    display.setBrightness(1);
     display.clear();
     start_time = millis();
     elapsed_time = millis() - start_time;
 }
 
 void loop(){
-    if(Serial){
-        recalibrateTime();
-    }
     elapsed_time = millis() - start_time;
 
-    if(elapsed_time > second_length){
+    if(elapsed_time >= second_length){
         sec += 1;
+        start_time = millis();
     }
 
     if(sec == 60){
